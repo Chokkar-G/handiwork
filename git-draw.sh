0bcb787881e470efc0ea25824589c9ddcfa9afd2
Chokkar G <chokkar.g@gmail.com>
2016-06-03 13:44:07 +0530

    git draw

---
+++ git-draw.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Run this script inside a new empty git repo.
+
+echo "Created with Git Draw (http://github.com/ben174/git-draw)" > README.md
+git add README.md
+touch gf
+git add gf
+echo 0 > gf
